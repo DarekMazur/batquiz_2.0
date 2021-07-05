7e7b721ded9f55c7a5f285884ee0c6733b6cc24a
Darek Mazur <dev@darekmazur.it>
2021-07-05 23:21:28 +0200

    setup initial state (hardcode)

--- src/components/QuizInit/QuizInit.js
+++ src/components/QuizInit/QuizInit.js
@@ -4,16 +4,17 @@ const QuizInit = (appBody) => {
 
   const quizTitle = document.createElement('h3');
   quizTitle.classList.add('quiz__title');
-  quizTitle.innerText = 'Title';
+  quizTitle.innerText = 'Welcome to BatQuiz! Please select game level and set your nickname.'; //initial title
 
   const quizBody = document.createElement('div');
   quizBody.classList.add('quiz__wrapper');
-  quizBody.innerText = 'Wrapper';
+  quizBody.innerHTML =
+    '<div><button>Easy</button><button>Normal</button><button>Hard</button></div><input /><div></div>'; //initial state
 
   const quizNav = document.createElement('div');
   quizNav.classList.add('quiz__navigation');
 
-  quizNav.innerText = 'Navigation';
+  quizNav.innerHTML = '<button>SEND</button><button>RESET</button>'; //initial state
 
   quizWrapper.appendChild(quizTitle);
   quizWrapper.appendChild(quizBody);
